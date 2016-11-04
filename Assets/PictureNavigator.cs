57eaf8e0e1eb922b7f4ce5087a0eb0478917273b
kil98q <killiandekok@gmail.com>
2016-11-04 12:39:07 +0100

    Stufff :dancer:

---
+++ Assets/PictureNavigator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PictureNavigator : MonoBehaviour {
+    public PictureSlider PictureSlider;
+    public Text _text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _text.text = (PictureSlider._CurrentPicture + 1).ToString() + "/" + (PictureSlider._AmountPictures+1).ToString();
+	}
+}
---
+++ Assets/PictureSlider.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class PictureSlider : MonoBehaviour {
+    [SerializeField]
+    private List<Picture> Pictures = new List<Picture>();
+    private int CurrentPicture;
+    public int _CurrentPicture
+    {
+        get { return CurrentPicture; }
+        private set { CurrentPicture = value; }
+    }
+    private int AmountPictures;
+    public int _AmountPictures
+    {
+        get { return AmountPictures; }
+        private set { AmountPictures = value;  }
+    }
+    [SerializeField]private Sprite[] Images;
+    public Image Display;
+
+
+    // Use this for initialization
+    void Start () {
+        
+        Images = Resources.LoadAll<Sprite>("Images");
+        foreach(Sprite _image in Images)
+        {
+            AddPicture(_image);
+            
+        }
+
+        AmountPictures = Pictures.Count;
+        Display.rectTransform.sizeDelta =new Vector2(Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Display.sprite = Pictures[CurrentPicture]._Picture;
+	}
+    public void Previous()
+    {
+        if (CurrentPicture>0)
+        {
+            CurrentPicture--;
+        }
+    }
+    public void Next()
+    {
+        if (CurrentPicture < Pictures.Count)
+        {
+            CurrentPicture++;
+        }
+    }
+    public void AddPicture(Sprite Picture)
+    {
+        Pictures.Add(new Picture(Picture));
+    }
+    
+}
+class Picture
+{
+    public Sprite _Picture;
+    public Picture(Sprite Picture)
+    {
+        _Picture = Picture;
+        
+    }
+}
+
